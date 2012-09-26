846c1bb07ded5a344aca47f33f3f2c0809fc5c32
hery <hery@hery-PC>
2012-09-27 01:30:01 +0200

    db_size is now compatible with postgresql

--- application/libraries/Administration.php
+++ application/libraries/Administration.php
@@ -43,16 +43,30 @@
 		
 		function db_size()
 		{
-			
-			$sql = 'SHOW TABLE STATUS';
-			
-			$query = $this->obj->db->query($sql);
-			$result = $query->result_array();
-			
-			foreach ($result as $row)
-			{
-				$db_size = $row['Data_length'] + $row['Index_length'];
-			}
+			switch ($this->obj->db->dbdriver)
+            {
+                case 'mysql':
+                    $sql = 'SHOW TABLE STATUS';
+                    
+                    $query = $this->obj->db->query($sql);
+                    $result = $query->result_array();
+                    
+                    foreach ($result as $row)
+                    {
+                        $db_size = $row['Data_length'] + $row['Index_length'];
+                    }
+                
+                break;
+                case 'postgre':
+                    $query = $this->obj->db->query("SELECT pg_size_pretty(pg_database_size('" . $this->obj->db->database . "')) as fulldbsize");
+                    
+                    $row = $query->row();
+                    $db_size = $row->fulldbsize;
+                break;
+                default:
+                    $db_size = 0;
+                break;
+            }
 			
 			return $db_size;
 			
